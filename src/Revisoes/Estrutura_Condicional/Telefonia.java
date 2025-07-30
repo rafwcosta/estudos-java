090b8e1988d35f90eb6d029c54ef2630fdfad97e
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-07-30 20:09:08 -0300

    Problema exemplo

---
+++ src/Revisoes/Estrutura_Condicional/Telefonia.java
@@ -0,0 +1,23 @@
+package Revisoes.Estrutura_Condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Telefonia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Minutos: ");
+        int minutos = sc.nextInt();
+
+        double conta = 50.0;
+        if (minutos > 100) {
+            conta += (minutos - 100) * 2.0;
+        }
+
+        System.out.printf("Valor a pagar: R$ %.2f%n", conta);
+
+        sc.close();
+    }
+}
