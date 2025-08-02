e4832fc889a0dd4a2867505dcda19e7ceed8819d
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-08-02 11:24:04 -0300

    Questão 2 - Estrutura repetitiva

---
+++ src/Revisoes/Estrutura_Repetitiva/Questao2.java
@@ -0,0 +1,35 @@
+package Revisoes.Estrutura_Repetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Questao2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite o valor de X: ");
+        int x = sc.nextInt();
+        System.out.print("Digite o valor de Y: ");
+        int y = sc.nextInt();
+
+        while (x != 0 && y != 0) {
+            if (x > 0 && y > 0) {
+                System.out.println("Primeiro.");
+            } else if (x < 0 && y > 0) {
+                System.out.println("Segundo.");
+            } else if (x < 0 && y < 0) {
+                System.out.println("Terceiro.");
+            } else {
+                System.out.println("Quarto");
+            }
+
+            System.out.print("Digite o valor de X: ");
+            x = sc.nextInt();
+            System.out.print("Digite o valor de Y: ");
+            y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
