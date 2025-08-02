c4f5d09b431bf91533c1aa4ec188e09305e41171
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-08-02 11:06:24 -0300

    Problema exemplo 1

---
+++ src/Revisoes/Estrutura_Repetitiva/Problema_Exemplo1.java
@@ -0,0 +1,26 @@
+package Revisoes.Estrutura_Repetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Problema_Exemplo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Para finalizar o cálculo, digite '0'");
+        System.out.print("Digite o valor de X: ");
+        int x = sc.nextInt();
+
+        int soma = 0;
+        while (x != 0) {
+            soma += x;
+            System.out.print("Digite o valor de X: ");
+            x = sc.nextInt();
+        }
+
+        System.out.println("Soma: " + soma);
+
+        sc.close();
+    }
+}
