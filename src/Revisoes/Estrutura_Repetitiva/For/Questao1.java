865a45ef95de0a6198a30aadd96d97a0b95545de
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-08-04 13:24:32 -0300

    Questão 1 - Estrutura repetitiva For

---
+++ src/Revisoes/Estrutura_Repetitiva/For/Questao1.java
@@ -0,0 +1,25 @@
+package Revisoes.Estrutura_Repetitiva.For;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Questao1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite a quantidade de valores a serem repetidos: ");
+        int n = sc.nextInt();
+
+        int soma = 0;
+        for (int i=0; i<n; i++) {
+            System.out.print("Digite o valor de X: ");
+            int x = sc.nextInt();
+            soma = soma + x;
+        }
+
+        System.out.println("Soma: " + soma);
+
+        sc.close();
+    }
+}
