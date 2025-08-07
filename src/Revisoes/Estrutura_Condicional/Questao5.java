e9fbf81fa9c1afb3bdaa47bb4fe373437f27402f
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-08-06 21:40:35 -0300

    Correções

--- src/Revisoes/Estrutura_Condicional/Questao5.java
+++ src/Revisoes/Estrutura_Condicional/Questao5.java
@@ -3,7 +3,6 @@ package Revisoes.Estrutura_Condicional;
 import java.util.Locale;
 import java.util.Scanner;
 
-/* Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas. */
 public class Questao5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
---
+++ src/Revisoes/Estrutura_Condicional/Questao7.java
@@ -0,0 +1,15 @@
+package Revisoes.Estrutura_Condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Questao7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        
+        
+
+        sc.close();
+    }
+}
