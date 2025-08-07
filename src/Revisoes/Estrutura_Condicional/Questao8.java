8db3f847c62ee0e09a50e46979f5d0fbc45d273d
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-08-06 21:53:17 -0300

    Questão 8 - Esrtrutura condicional

---
+++ src/Revisoes/Estrutura_Condicional/Questao8.java
@@ -0,0 +1,33 @@
+package Revisoes.Estrutura_Condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Questao8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        
+        System.out.print("Salário: ");
+        double salario = sc.nextDouble();
+
+        double imposto;
+        if (salario <= 2000.0) {
+			imposto = 0.0;
+		} else if (salario <= 3000.0) {
+			imposto = (salario - 2000.0) * 0.08;
+		} else if (salario <= 4500.0) {
+			imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+		} else {
+			imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+		}
+
+		if (imposto == 0.0) {
+			System.out.println("Isento");
+		} else {
+			System.out.printf("R$ %.2f%n", imposto);
+		}
+
+        sc.close();
+    }
+}
