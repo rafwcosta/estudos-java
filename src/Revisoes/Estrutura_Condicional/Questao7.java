25693782b57581d2470a97a782203234f157292b
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-08-06 21:42:48 -0300

    Questão 7 - Estrutura condicional

--- src/Revisoes/Estrutura_Condicional/Questao7.java
+++ src/Revisoes/Estrutura_Condicional/Questao7.java
@@ -8,7 +8,22 @@ public class Questao7 {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
         
-        
+        System.out.print("X: ");
+        double x = sc.nextDouble();
+        System.out.print("Y: ");
+        double y = sc.nextDouble();
+
+        if (x == 0 && y == 0) {
+            System.out.println("Origem.");
+        } else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else {
+            System.out.println("Q4");
+        }
 
         sc.close();
     }
