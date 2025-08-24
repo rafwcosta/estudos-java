610ece0c42d61bb3a18e7fa5e577ee06a6758af4
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-08-24 16:01:34 -0300

    Questão 1 - While

---
+++ src/Revisoes/Estrutura_Repetitiva/While/Questao1.java
@@ -0,0 +1,24 @@
+package Revisoes.Estrutura_Repetitiva.While;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Questao1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite a senha: ");
+        int senha = sc.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("Senha inválida!");
+            System.out.print("Digite a senha: ");
+            senha = sc.nextInt();
+        }
+
+        System.out.println("Acesso permitido!");
+
+        sc.close();
+    }
+}
